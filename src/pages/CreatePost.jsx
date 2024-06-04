31c6ccc5fc870b922899727d0b835d8f6973e344
Karan Dixit <karandixit10@gmail.com>
2024-06-05 03:59:14 +0530

    Finished Creatign CreatePost And FormFields

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -6,23 +6,35 @@ import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
   const navigate = useNavigate();
+
   const [form, setForm] = useState({
     name: "",
-    promt: "",
+    prompt: "",
     photo: "",
   });
+
   const [generatingImage, setGeneratingImage] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = () => {}
+  const generateImage = () => {};
 
-  const handleChange = (e) => {}
+  const handleSubmit = () => {};
 
+  const handleChange = (e) => {
+    setForm({
+     ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   //call the utility function to always get new prompt
   const handleSurpriseMe = () => {
-
-  }
+    const randomPropmt = getRandomPrompt(form.prompt);
+    setForm({
+     ...form,
+      prompt: randomPropmt,
+    });
+  };
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -40,7 +52,7 @@ const CreatePost = () => {
             labelName="Your Name"
             type="text"
             name="name"
-            placeholder="DALLA-AI"
+            placeholder="Ex., john doe"
             value={form.name}
             handleChange={handleChange}
           />
@@ -48,12 +60,54 @@ const CreatePost = () => {
             labelName="Prompt"
             type="text"
             name="prompt"
-            placeholder="a bowl of soup that looks like a monster, knitted out of wool"
+            placeholder="A bowl of soup that looks like a monster, knitted out of wool"
             value={form.prompt}
             handleChange={handleChange}
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.photo}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt={preview}
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+
+            {generatingImage && (
+              <div className="absolute insert-0 z-0 flex justify-center items-center bg-[rgb(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {generatingImage ? "Generating..." : "Generate"}
+          </button>
+        </div>
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">
+            ** Once you have created the image you want, you can share it with
+            others in the community **
+          </p>
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? "Sharing..." : "Share with the Community"}
+          </button>
         </div>
       </form>
     </section>
