e9211ae78b99b13db912e1340ba49330671b74b0
Karan Dixit <karandixit10@gmail.com>
2024-06-05 05:55:13 +0530

    Update createPost to post to cloudinary to store images

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -16,13 +16,65 @@ const CreatePost = () => {
   const [generatingImage, setGeneratingImage] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const generateImage = () => {};
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImage(true);
+        const response = await fetch("http://localhost:8080/api/v1/dalle", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt: form.prompt,
+          }),
+        });
 
-  const handleSubmit = () => {};
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (err) {
+        alert(err);
+      } finally {
+        setGeneratingImage(false);
+      }
+    } else {
+      alert("Please provide proper prompt");
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          "http://localhost:8080/api/v1/post",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ ...form }),
+          }
+        );
+
+        await response.json();
+        alert("Success");
+        navigate("/");
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert("Please generate an image with proper details");
+    }
+  };
 
   const handleChange = (e) => {
     setForm({
-     ...form,
+      ...form,
       [e.target.name]: e.target.value,
     });
   };
@@ -31,7 +83,7 @@ const CreatePost = () => {
   const handleSurpriseMe = () => {
     const randomPropmt = getRandomPrompt(form.prompt);
     setForm({
-     ...form,
+      ...form,
       prompt: randomPropmt,
     });
   };
