5591d7c9f3e7fb5f8f52fcd025b208e4253c63ff
Karan Dixit <karandixit10@gmail.com>
2024-06-05 21:50:29 +0530

    Change endpoints of the api

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImage(true);
-        const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        const response = await fetch("https://dalle-e-image-generation-backend.onrender.com/api/v1/dalle", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -52,7 +52,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/posts", {
+        const response = await fetch("https://dalle-e-image-generation-backend.onrender.com/api/v1/posts", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
 
     try {
       const response = await fetch(
-        "http://localhost:8080/api/v1/posts",
+        "https://dalle-e-image-generation-backend.onrender.com/api/v1/posts",
         {
           method: "GET",
           headers: {
